119b8d1f5a97f3f049e02f7e9ffb6c896b814c44
Ayesha Misba <ayeshamisba2001@gmail.com>
2021-12-26 23:34:15 +0530

    day 2

---
+++ day2/Program1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Program1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
+        	int n = sc.nextInt();
+        		for (int j = 0; j < n; j++) {
+        			a += (int) Math.pow(2, j)*b;
+        			System.out.print(a +" ");
+        		}
+        	System.out.println();
+        }
+    }
+}
